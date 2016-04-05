192ff24d9cea9f372fd493df606d27644ae7bb0c
syedfarrukhqamar <syedfarrukhqamar@yahoo.com>
2016-04-05 15:22:07 +0200

    Initial Commit

---
+++ SLife/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SLife
+//
+//  Created by syed farrukh Qamar on 05/04/16.
+//  Copyright © 2016 Be My Competence AB. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
