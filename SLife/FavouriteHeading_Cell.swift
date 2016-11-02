44023608d435f04b2369e2acb1f48f31b9efc592
syedfarrukhqamar <syedfarrukhqamar@yahoo.com>
2016-11-02 06:49:40 +0100

    Old Files

---
+++ SLife/FavouriteHeading_Cell.swift
@@ -0,0 +1,24 @@
+//
+//  FavouriteHeading_Cell.swift
+//  SLife
+//
+//  Created by syed farrukh Qamar on 04/08/16.
+//  Copyright © 2016 Be My Competence AB. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteHeading_Cell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ TripSuggestionHeaderCell.swift
@@ -0,0 +1,28 @@
+//
+//  TripSuggestionHeaderCell
+//  SLife
+//
+//  Created by syed farrukh Qamar on 25/04/16. 1715hrs
+//  Copyright © 2016 Be My Competence AB. All rights reserved.
+//
+
+import UIKit
+
+class TripSuggestionHeaderCell: UITableViewCell {
+    
+@IBOutlet weak var headerLabel: UILabel!
+    
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
